41540cd225771b64dda8bbc5b64c3aac9717b570
ignaciodrd <ignaciodrd@gmail.com>
2022-09-29 11:35:25 -0400

    ecma ES11

---
+++ src/es11/00-optional-chaning.js
@@ -0,0 +1,12 @@
+const users = {
+  igdrd: {
+    country: 'PY'
+  },
+  ana: {
+    country: 'SP'
+  }
+};
+
+console.log(users.igdrd);
+console.log(users.ana.country);
+console.log(users.igdrd?.age);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 999989895626398599999n;
+const anotherBigNumber = BigInt(999989895626398599999);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
